c40142a1acc4ed6026ad21919402acb54db28b8d
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-04-27 09:26:25 +0100

    Create demo.rb and add simple explanations

---
+++ demo.rb
@@ -0,0 +1,44 @@
+# Proc
+module Enumerable
+
+  def my_each(proc)
+    for i in self
+      proc.call i
+    end
+  end
+
+end
+
+proc = Proc.new do |i|
+  puts i
+end
+
+[1,2,3,4,5].my_each(proc)   #=> displays 1, 2, 3, 4, 5 on new lines respectively
+  
+
+# Block
+module Enumerable
+
+  def my_each(&block)
+    for i in self
+      block.call i
+    end
+  end
+
+end
+
+[1,2,3,4,5].my_each {|n| puts n }   #=> displays 1, 2, 3, 4, 5 on new lines respectively
+  
+
+# Yield
+module Enumerable
+
+  def my_each
+    for i in self
+      yield i
+    end
+  end
+
+end
+
+[1,2,3,4,5].my_each {|n| puts n }   #=> displays 1, 2, 3, 4, 5 on new lines respectively
